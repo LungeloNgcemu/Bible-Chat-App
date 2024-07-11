c61f92fe418742e8d744797e9cf2877ea1c2ba11
Lungelo Ngcemu <lungelofarm@gmail.com>
2024-07-11 18:49:12 +0200

    first

---
+++ index.js
@@ -0,0 +1,11 @@
+var button1 = document.getElementById("app1");
+
+var button2 = document.getElementById("app2");
+
+button1.addEventListener("click", () => {
+    alert("IOS Applicatiion Will be available soon");
+});
+
+button2.addEventListener("click", () => {
+    alert("Andriod Applicatiion Will be available soon");
+});
